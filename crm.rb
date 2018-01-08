a4b5c12dfa784062c6168af8af6d4f2ce515003e
jeffrothwell <rothwelljeff@gmail.com>
2018-01-08 14:35:14 -0500

    complete update contact routes

--- crm.rb
+++ crm.rb
@@ -28,14 +28,39 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-  get '/contacts/:id' do
-    @contact = Contact.find(params[:id])
-    if @contact
-      erb(:show_contact)
-    else
-      raise Sinatra::NotFound
-    end
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  if @contact
+    erb(:show_contact)
+  else
+    raise Sinatra::NotFound
+  end
 
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 after do
